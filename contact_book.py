707af8c9c5e081c2969aac0d8bc2c74d9d68c704
PREETI KUMARI <preetikumari.246006@gmail.com>
2026-04-24 18:57:11 +0500

    Implement basic contact book menu

---
+++ contact_book.py
@@ -0,0 +1,20 @@
+while True:
+    print("===== Contact Book =====")
+    print("1. Add Contact ")
+    print("2. View All Contacts")
+    print("3. Search Contact")
+    print("4. Delete Contact")
+    print("5. Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        print("1.Adding number......")
+    elif choice == "2":
+        print("2.Viewing contact.......")
+    elif choice == "3":
+        print("3.Searching contact.......")
+    elif choice == "4":
+        print("4.Deleting number.......")
+    elif choice == "5":
+        break
+    else:
+        print("Invalid choice! Enter 1-5")
