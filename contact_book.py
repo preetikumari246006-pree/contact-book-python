561c3dab07e1ad34474f6262089235226af06d86
PREETI KUMARI <preetikumari.246006@gmail.com>
2026-04-25 17:20:02 +0500

    Fix formatting and improve print statements

--- contact_book.py
+++ contact_book.py
@@ -1,26 +1,28 @@
 contacts = []
 while True:
     print("===== Contact Book =====")
-    print("1. Add Contact ")
+    print("1. Add Contact")
     print("2. View All Contacts")
     print("3. Search Contact")
     print("4. Delete Contact")
     print("5. Exit")
     choice = input("Enter your choice: ")
-    
     if choice == "1":
-        print("1.Adding number......")
         name = input("Enter your name: ")
         num = input("Enter your number: ")
         contact = {"name": name, "number": num}
         contacts.append(contact)
         print("Contact saved Successfully...")
     elif choice == "2":
-        print("2.Viewing contact.......")
+        if len(contacts) == 0:
+                print("No contacts found!")
+        else:
+            for contact in contacts:
+                print("Name:", contact["name"], "| Number:", contact["number"])
     elif choice == "3":
-        print("3.Searching contact.......")
+        print("3. Searching contact.......")
     elif choice == "4":
-        print("4.Deleting number.......")
+        print("4. Deleting number.......")
     elif choice == "5":
         break
     else:
